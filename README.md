073dd464a20200ee1b94d848423ef2f5ced99161
Staroz <marcourquidi17@gmail.com>
2023-04-25 16:05:34 -0400

    fix to MU 07, again

--- README.md
+++ README.md
@@ -4,10 +4,16 @@ automation training
 for test "testTrello.cy.js"
 
 In  the file  cypress.config.js, fill in the fields of the object "credentials"
-whit 
-mar0cou0rqu0idi017@0gmail.com and hij0ode0cai0n170
-on email and pw respectively.
+with:
+``` 
+env: {
+      credentials: {
+        email: "example@email.com",
+        pw: "MyPassword",
+      }
+}
+```
 
 
 
-PD: remove "0" for introducing this data.
+
--- cypress/e2e/testTrello.cy.js
+++ cypress/e2e/testTrello.cy.js
@@ -1,13 +1,13 @@
 /// <reference types= "cypress" />
 
 
-describe('created a new board', function(){
+describe('Boards', function(){
 
     before(()=> {
         cy.login(Cypress.env())
 
         });
-    it('created board', () => {
+    it('Create a new board', () => {
         cy.get('li[data-testid="home-team-tab-section-6442879b62c449644bea42b0"] span[data-testid="DownIcon"]').click();
         cy.get('[data-testid="home-team-boards-tab"] > .DD3DlImSMT6fgc').click();
         cy.get('.QB_5E6Ho6209bY > .bxgKMAm3lq5BpA').click();
