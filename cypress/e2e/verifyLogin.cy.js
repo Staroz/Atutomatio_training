aa9be700cd76e6855c0ff3ab1d8f0673c995c7c3
Staroz <marcourquidi17@gmail.com>
2023-04-26 00:39:12 -0400

    Fix to (task 03 for review)

--- cypress/e2e/verifyLogin.cy.js
+++ cypress/e2e/verifyLogin.cy.js
@@ -3,11 +3,11 @@
 describe('Validate', function(){
     
     before(()=> {
-        cy.login(Cypress.env())
+        cy.login();
         
         });
     it('Validate that the new page is loaded', () => {
-        cy.get('.DweEFaF5owOe02').click()
-        cy.get('.lzFtVDCea8Z9jO').should('be.visible')
+        cy.get('.DweEFaF5owOe02').click();
+        cy.get('.lzFtVDCea8Z9jO').should('contain', 'staroz');
     });
 })
