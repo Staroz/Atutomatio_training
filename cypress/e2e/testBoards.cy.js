37efbe3cebfd6ba61cb8a6a4f67f86d7f66113cc
Staroz <marcourquidi17@gmail.com>
2023-05-29 03:02:30 -0400

    Create and delete a workspace in Trello

--- cypress/e2e/testBoards.cy.js
+++ cypress/e2e/testBoards.cy.js
@@ -1,6 +1,6 @@
 /// <reference types= "cypress" />
 
-describe("Boards", function() {
+describe("Manipulating UI of Trello", function() {
     before(function() {
         cy.fixture("credentials1.json").as('credentials');
     });
@@ -9,25 +9,48 @@ describe("Boards", function() {
         cy.login(this.credentials.email, this.credentials.pw, this.credentials.userName);
 	});
 
-	describe("Create", function () {
-		it("Create a new board", function () {
+    after(function() {
+        cy.visit('/u/'+this.credentials.userName +'/boards');
+        cy.logout();
+    });
+
+    describe("Create a work space whit UI", function () {
+		it("Create WS", function () {
+				cy.visit(`/u/" + ${this.credentials.userName} + "/boards`);
+				cy.createWorkSpace(this.credentials.workSpaceName);
+                cy.get('[data-testid="home-team-tab-name"]').contains(this.credentials.workSpaceName).should('be.visible');
+		});
+
+        after(function () {
+            cy.getWorkSpacedId(this.credentials.key, this.credentials.token, this.credentials.workSpaceName);
+            cy.workSpaceDeleteApi(this.credentials.key, this.credentials.token);
+        });
+	});
+
+	describe("Create a new board whit UI", function () {
+        before(function() {
+			cy.workSpaceCreateApi(this.credentials.key, this.credentials.token, this.credentials.workSpaceName);   
+        });
+        it("Create board", function () {
 				cy.visit(`/u/" + ${this.credentials.userName} + "/boards`);
 				cy.createBoard(this.credentials.boardName);
-				cy.get('[data-testid="board-name-display"]').
-                    should('have.text', this.credentials.boardName);
+				cy.get('[data-testid="board-name-display"]')
+                    .should('have.text', this.credentials.boardName);
 		});
 
         after(function () {
             cy.getBoardId(this.credentials.key, this.credentials.token, this.credentials.boardName);
             cy.boardDeleteApi(this.credentials.key, this.credentials.token);
+            cy.workSpaceDeleteApi(this.credentials.key, this.credentials.token);
         });
 	});
 
-    describe('Modify', function () {
+    describe('Update board name whit UI', function () {
 		before(function() {
-			cy.boardCreateApi(this.credentials.key, this.credentials.token, this.credentials.boardName);   
+			cy.workSpaceCreateApi(this.credentials.key, this.credentials.token, this.credentials.workSpaceName);
+            cy.boardCreateApi(this.credentials.key, this.credentials.token, this.credentials.boardName);   
         });
-        it('Modify the title of the board', function() {
+        it('Update board', function() {
 				cy.visit('/u/'+ this.credentials.userName +'/boards');
                 cy.modifyBoardName(this.credentials.boardName, this.credentials.newBoardName);
 				cy.get('[data-testid="board-name-display"]')
@@ -37,18 +60,23 @@ describe("Boards", function() {
         after(function (){
             cy.getBoardId(this.credentials.key, this.credentials.token, this.credentials.newBoardName);
             cy.boardDeleteApi(this.credentials.key, this.credentials.token);
+            cy.workSpaceDeleteApi(this.credentials.key, this.credentials.token);
         });
 	});
-	describe("Delete", function () {
+
+	describe("Delete a board whit UI", function () {
 		before(function() {
+            cy.workSpaceCreateApi(this.credentials.key, this.credentials.token, this.credentials.workSpaceName);
             cy.boardCreateApi(this.credentials.key, this.credentials.token, this.credentials.boardName);   
             });
 		it("Delete a Board", function() {
 			cy.visit('/u/'+this.credentials.userName +'/boards');
             cy.boardDelete(this.credentials.boardName);
+            cy.get('[class="board-tile-details-name"]').should('not.exist');
 		});
-	});
-    after(() => {
-        cy.logout();
+
+        after(function() {
+            cy.workSpaceDeleteApi(this.credentials.key, this.credentials.token);
+        });
 	});
 });
