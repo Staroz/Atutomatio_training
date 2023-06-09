a16616ca5a2b40873456238dc5b8d1729ea9da27
Staroz <marcourquidi17@gmail.com>
2023-06-09 03:57:46 -0400

    Test with cards in Trello

--- cypress/e2e/testBoards.cy.js
+++ cypress/e2e/testBoards.cy.js
@@ -55,7 +55,6 @@ describe("Boards", function() {
 		it("Delete a Board", function() {
 			cy.visit('/u/'+this.credentials.userName +'/boards');
             cy.boardDelete(this.credentials.boardName);
-            cy.wait(1500);
             cy.get('[data-testid="board-name-display"]').should('not.exist');
 		});
 	});
