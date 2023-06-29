ebd3e47d9f6bf58863dc984d599f639548e75a0b
Staroz <marcourquidi17@gmail.com>
2023-06-29 02:08:20 -0400

    Uploading github secrets

--- cypress/e2e/archiveList.cy.js
+++ cypress/e2e/archiveList.cy.js
@@ -1,6 +1,6 @@
 /// <reference types= "cypress" />
 
-describe("Copy a cards in another list", function() {
+describe("Archive a list of a board, with more than one list", function() {
     before(function() {
         cy.fixture("credentials1.json").as('credentials');
     });
@@ -17,18 +17,17 @@ describe("Copy a cards in another list", function() {
         cy.logout(this.credentials.userName);
     });
 
-    describe("Copy a card", function () {
+    describe("Archive a list", function () {
         before(function() {
-            //Creating workspace, board, list and card with API.
+            //Creating workspace, board, list with API.
             cy.workSpaceCreateApi(this.credentials.key, this.credentials.token, this.credentials.workSpaceName);
             cy.boardCreateApi(this.credentials.key, this.credentials.token, this.credentials.boardName);
             cy.createListsApi(this.credentials.key, this.credentials.token, this.credentials.listNameArray); 
-            cy.createCardApi(this.credentials.key, this.credentials.token, [this.credentials.cardsNameArray[0]]); 
         });
-        it("Copy Card 01 to In Progress list", function () {
-            cy.joinBoard(this.credentials.userName, this.credentials.boardName);
-            cy.copyCard(this.credentials.cardsNameArray[0], this.credentials.copiedCardInfo.copyCardName, this.credentials.boardName, this.credentials.listNameArray[1], this.credentials.copiedCardInfo.positionOfCard);
-            cy.get('[data-testid="list"]').contains('[class="list js-list-content"]', this.credentials.listNameArray[1]).children().should('contain.text', this.credentials.copiedCardInfo.copyCardName);
+        it("Archive", function () {
+            cy.visit('/u/'+this.credentials.userName +'/boards');
+            cy.archiveList(this.credentials.boardName, this.credentials.listNameArray[1]);
+            cy.get('[class="list js-list-content"]').should('not.contain.text' , this.credentials.listNameArray[1]);
         });
     });
 });
