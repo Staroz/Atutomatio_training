4f5a0d7b1815ef3872ce9870bff1d736fb0bd0d1
Staroz <marcourquidi17@gmail.com>
2023-07-04 20:33:03 -0400

    Test allure report #1

--- tests/workspaces.spec.js
+++ tests/workspaces.spec.js
@@ -33,7 +33,7 @@ test.describe('Testing workspaces in Trello', async () => {
     test.describe('Create a workspace', async () => {
         test('Create', async () => {
         await workspaceUi.createWorkspace(credentials.workSpaceName);
-        await expect(workspaceUi.selectWorkspaceBtn).toContainText([credentials.workSpaceName]);
+        await expect(workspaceUi.selectWorkspaceBtn).toContainText('asdre');
         });
 
         test.afterEach(async () => {
