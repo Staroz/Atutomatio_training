d4ba3e821cde0c209f0bfc8f457f8afebd4c2677
Staroz <marcourquidi17@gmail.com>
2023-07-10 02:06:26 -0400

    test mu_23  config file

--- playwright.config.js
+++ playwright.config.js
@@ -11,15 +11,15 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 40 *1000,
+  timeout: 60 *1000,
   expect: {timeout: 10000},
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -27,12 +27,11 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
+
     baseURL: 'https://trello.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+
   },
 
   /* Configure projects for major browsers */
@@ -41,7 +40,11 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
             headless: true,
-            screenshot: 'on'
+            screenshot: 'only-on-failure',
+            video: {
+              mode: 'on-first-retry', 
+              size: { width: 840, height: 680 }
+            }
           },
     },
     // {
